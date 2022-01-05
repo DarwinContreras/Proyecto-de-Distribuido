683154e9b43ddf40189c112f566adaa5b6588771
Darwin Contreras <46517680+DarwinContreras@users.noreply.github.com>
2022-01-05 05:45:11 -0500

    Add files via upload

---
+++ Proyecto_floristeria.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_floristeria;
+
+import proyecto_floristeria.vista.login;
+
+/**
+ *
+ * @author pc
+ */
+public class Proyecto_floristeria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login v = new login();
+        v.setVisible(true);
+    }
+    
+}
